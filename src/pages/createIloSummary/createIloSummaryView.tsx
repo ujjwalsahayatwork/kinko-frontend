2f9037e93ccb874f32d1fbe3830dfb407c788f3b
sourabh-chouhan <sourabh.thoughtwin@gmail.com>
2022-07-04 15:29:22 +0530

    worked on Summary page

--- src/pages/createIloSummary/createIloSummaryView.tsx
+++ src/pages/createIloSummary/createIloSummaryView.tsx
@@ -35,7 +35,7 @@ const ColorRect = styled.div<{ colorIndex: number }>`
 	background-color: ${({ theme, colorIndex }) => theme.doughnutColors[colorIndex]};
 	height: 30px;
 	border-radius: 5px;
-	width: 250px;
+	width: 120px;
 	@media (max-width: ${({ theme }) => toPx(theme.mobileThreshold)}) {
 		height: 25px;
 		width: 260px;
@@ -45,13 +45,25 @@ const ColorRect = styled.div<{ colorIndex: number }>`
 
 const NextButton = styled(Button)`
 	width: 170px;
+	background-color: #F97A48;
+	border: 1px solid #F97A48;
+	height: 47px;
+	padding: 10px 20px 10px 0px;
+	text-align: center;
+`;
+const BackButton = styled(Button)`
+	width: 170px;
+	border: 1px solid #F97A48;
+	height: 47px;
 	padding: 10px 20px 10px 0px;
 	text-align: center;
 `;
 
 const Forcolor = styled.div`
 	display: flex;
+	flex-direction: column;
 	width: 350px;
+	gap: 10px;
 	color: white;
 	font-size: 18px;
 	justify-content: space-between;
@@ -78,8 +90,13 @@ const StyledCardRow = styled(Row)`
 	gap: 1rem;
 `;
 
+const ButtonsContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
 const StyledCard = styled(Col)`
-	background: #252123;
+	background: #002545;
 	border: 1px solid #4B4B4B;
 	border-radius: 5px;
 	padding: 1rem;
@@ -134,101 +151,97 @@ export const CreateIloSummaryView: FC<ICreateIloSummaryViewProps> = ({
 
 
 			<Spacing vertical="xl" mobile="l" />
-			<Row justify="space-between" mobileDirection="column">
-				<Col mobileDirection="column-reverse">
-					<Row mobileDirection="row-reverse" justify={isMobile ? 'center' : undefined}>
-						<Col justify="space-between">
-							<Row align="center">
-								<Forcolor>
-									Presale
-									<Spacing horizontal="s" />
-									<ColorRect colorIndex={0} />
-								</Forcolor>
-							</Row>
-							<Spacing vertical="m" />
-							<Row align="center">
-								<Forcolor>
-									Liquidity
-									<Spacing horizontal="s" />
-									<ColorRect colorIndex={1} />
-								</Forcolor>
-							</Row>
-							<Spacing vertical="m" />
+			{/* <Row justify="space-between" mobileDirection="column"> */}
+			<Col mobileDirection="column-reverse">
+				<Row mobileDirection="row-reverse" justify={isMobile ? 'center' : undefined}>
+					<Row justify="space-between">
+						<Row align="center">
+							<Forcolor>
+								Presale
+								<ColorRect colorIndex={0} />
+							</Forcolor>
+						</Row>
+						<Spacing vertical="m" />
+						<Row align="center">
+							<Forcolor>
+								Liquidity
+								<ColorRect colorIndex={1} />
+							</Forcolor>
+						</Row>
+						<Spacing vertical="m" />
 
-							<Row align="center">
-								<Forcolor>
-									Fees
-									<Spacing horizontal="s" />
-									<ColorRect colorIndex={2} />
-								</Forcolor>
-							</Row>
-							<Spacing vertical="m" />
-							<Row align="center">
-								<Forcolor>
-									Free
-									<Spacing horizontal="s" />
-									<ColorRect colorIndex={3} />
-								</Forcolor>
-							</Row>
-							<Spacing vertical="m" />
-						</Col>
-					</Row>
-				</Col>
-				<Spacing horizontal="m" vertical="l" />
-				<StyledCardBox>
-					<StyledCardRow>
-						<StyledCard>
-							<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
-								Total {saleTokenSymbol} required
-							</Text>
-							<Spacing vertical="s" />
-							<Text fontSize="l" fontWeight="bold" color="secondaryBrand">
-								{saleTokenTotalRequired.toFixed(3)} {saleTokenSymbol}
-							</Text>
-							<Spacing vertical="s" />
-						</StyledCard>
-						<StyledCard>
-							<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
-								Amount for sale
-							</Text>
-							<Spacing vertical="s" />
-
-							<Text fontSize="l" fontWeight="bold" color="secondaryBrand">
-								{presaleAmount.toFixed(3)} {saleTokenSymbol}
-							</Text>
-							<Spacing vertical="s" />
-						</StyledCard>
-					</StyledCardRow>
-					{
-						isDesktop && <Spacing vertical="m" horizontal="m" mobile="s" />
-					}
-					<StyledCardRow>
-						<StyledCard>
-							<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
-								Amount for liquidity
-							</Text>
-							<Spacing vertical="s" />
-
-							<Text fontSize="l" fontWeight="bold" color="secondaryBrand">
-								{liquidityAmount.toFixed(3)} {saleTokenSymbol}
-							</Text>
-							<Spacing vertical="s" />
-						</StyledCard>
-						<StyledCard>
-							<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
+						<Row align="center">
+							<Forcolor>
 								Fees
-							</Text>
-							<Spacing vertical="s" />
+								<ColorRect colorIndex={2} />
+							</Forcolor>
+						</Row>
+						<Spacing vertical="m" />
+						<Row align="center">
+							<Forcolor>
+								Free
+								<ColorRect colorIndex={3} />
+							</Forcolor>
+						</Row>
+						<Spacing vertical="m" />
+					</Row>
+				</Row>
+			</Col>
+			<Spacing horizontal="m" vertical="l" />
+			<StyledCardBox>
+				<StyledCardRow>
+					<StyledCard>
+						<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
+							Total {saleTokenSymbol} required
+						</Text>
+						<Spacing vertical="s" />
+						<Text fontSize="l" fontWeight="bold" color="greeny">
+							{saleTokenTotalRequired.toFixed(3)} {saleTokenSymbol}
+						</Text>
+						<Spacing vertical="s" />
+					</StyledCard>
+					<StyledCard>
+						<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
+							Amount for sale
+						</Text>
+						<Spacing vertical="s" />
 
-							<Text fontSize="l" fontWeight="bold" color="secondaryBrand">
-								{feesAmount.toFixed(3)} {saleTokenSymbol}
-							</Text>
-							<Spacing vertical="s" />
+						<Text fontSize="l" fontWeight="bold" color="greeny">
+							{presaleAmount.toFixed(3)} {saleTokenSymbol}
+						</Text>
+						<Spacing vertical="s" />
+					</StyledCard>
+				</StyledCardRow>
+				{
+					isDesktop && <Spacing vertical="m" horizontal="m" mobile="s" />
+				}
+				<StyledCardRow>
+					<StyledCard>
+						<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
+							Amount for liquidity
+						</Text>
+						<Spacing vertical="s" />
 
-						</StyledCard>
-					</StyledCardRow>
-				</StyledCardBox>
-			</Row>
+						<Text fontSize="l" fontWeight="bold" color="greeny">
+							{liquidityAmount.toFixed(3)} {saleTokenSymbol}
+						</Text>
+						<Spacing vertical="s" />
+					</StyledCard>
+					<StyledCard>
+						<Text fontSize="s" fontWeight="normal" whiteSpace="nowrap" className='mob_text_size'>
+							Fees
+						</Text>
+						<Spacing vertical="s" />
+
+						<Text fontSize="l" fontWeight="bold" color="greeny">
+							{feesAmount.toFixed(3)} {saleTokenSymbol}
+						</Text>
+						<Spacing vertical="s" />
+
+					</StyledCard>
+				</StyledCardRow>
+			</StyledCardBox>
+			{/* </Row> */}
 			{showDEVIssue && (
 				<>
 					<Spacing vertical="xl" mobile="m" />
@@ -241,9 +254,10 @@ export const CreateIloSummaryView: FC<ICreateIloSummaryViewProps> = ({
 			)}
 			<Spacing vertical="l" />
 
-			<Row justify={isDesktop ? 'flex-end' : 'center'}>
-				<NextButton label="Next to Socials" arrow onClick={onSubmit} />
-			</Row>
+			<ButtonsContainer>
+				<BackButton label="Back" onClick={onSubmit} />
+				<NextButton label="Next to Caps" arrow onClick={onSubmit} />
+			</ButtonsContainer>
 			<Spacing vertical="xl" />
 		</StyledMainCol>
 	);
