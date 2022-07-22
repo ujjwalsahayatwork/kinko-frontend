a78860b2faf6645434c7aeefc429e11a6fcb8508
chouhan-vijay <vijay.thoughtwin@gmail.com>
2022-07-22 15:19:08 +0530

    Ilo card hover color changed

--- src/pages/main/components/iloCard.tsx
+++ src/pages/main/components/iloCard.tsx
@@ -15,6 +15,23 @@ import iloImage from '../../../assets/images/demoImage.png';
 
 const StyledCard = styled(Col)`
 	margin: 10px;
+	border: 1px solid transparent;
+	:hover {
+		border: 1px solid #4650a8;
+		border-radius: 0.8rem;
+		.card_bg {
+			background: #4650a8 !important;
+			border: 1px solid #4650a8;
+			border-top: none;
+		}
+		.progress_bar {
+			background: #120d25 !important;
+		}
+		.divider {
+			background: #ffffff !important;
+			mix-blend-mode: overlay;
+		}
+	}
 `;
 
 const StyledHeaderRow = styled(Row)``;
@@ -44,7 +61,7 @@ const TitleWrapper = styled(Row)`
 
 const StyledCardBackground = styled.div`
 	background: #1b1632;
-	height: 11.7rem;
+	height: 12.9rem;
 	border: 1px solid #3f3955;
 	border-top: none;
 	border-bottom-left-radius: 0.7rem;
@@ -58,7 +75,7 @@ const StyledDivider = styled(Col)`
 
 const StyledProgressBar = styled(Row)`
 	background: #3f3955;
-	height: 2.6rem;
+	height: 2.5rem;
 	border-bottom-left-radius: 0.7rem;
 	border-bottom-right-radius: 0.7rem;
 `;
@@ -67,7 +84,6 @@ const IloDescription = styled(Row)`
 	min-height: 4rem;
 `;
 const IloDuration = styled(Text)`
-	font-family: 'Sora';
 	font-style: normal;
 	font-weight: 300;
 	font-size: 14px;
@@ -76,7 +92,6 @@ const IloDuration = styled(Text)`
 	text-transform: uppercase;
 `;
 const IloDays = styled(Text)`
-	font-family: 'Sora';
 	font-style: normal;
 	font-weight: 700;
 	font-size: 14px;
@@ -94,6 +109,28 @@ const CardInfo = styled(Row)`
 
 const StatusBar = styled(Row)`
 	justify-content: space-between;
+	align-items: center;
+`;
+
+const StatusCol = styled(Col)`
+	border: 1px solid #6fcf97;
+	border-radius: 5px;
+`;
+
+const StatusText = styled(Text)`
+	padding: 0.3rem;
+	font-family: 'Sora';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 12px;
+	line-height: 15px;
+	text-align: center;
+`;
+
+const StyledStatusRow = styled(Row)`
+	background: #120d25;
+	border-radius: 10px;
+	padding: 0.7rem;
 `;
 
 interface IIloCardProps {
@@ -122,7 +159,7 @@ export const IloCard: FC<IIloCardProps> = ({ data, width }) => {
 	return (
 		<RouterLink to={`/ilo/${launchpadAddress}`}>
 			<StyledCard className="card_space">
-				<StyledHeaderRow className="@@@@@@@@@@" align="center" justify="flex-end">
+				<StyledHeaderRow align="center" justify="flex-end">
 					<Header fontSize="xxs" fontWeight="bold" justify="center" transform="uppercase">
 						{status === 'upcoming'
 							? `starts ${startBlockDate.toLocaleString()} your time`
@@ -141,54 +178,46 @@ export const IloCard: FC<IIloCardProps> = ({ data, width }) => {
 				</StyledHeaderRow>
 				<Image src={`${IMAGES_URL}/${headerImageFileName}`} height={242} width={width} />
 				{/* <Image src={`${iloImage}`} height={242} width={width} /> */}
-				<StyledCardBackground>
+				<StyledCardBackground className="card_bg">
 					<TitleWrapper justify="space-between" align="center">
 						<Text fontSize="m" fontWeight="bold">
 							{iloName}
 						</Text>
 						<Icon icon="angleRight" color="secondaryBrand" height={13} />
 					</TitleWrapper>
-					<StyledDivider />
+					<StyledDivider className="divider" />
 					<IloDescription justify="space-between" horizontalPadding="m" verticalPadding="m">
 						<CardInfo>
 							<StatusBar>
 								<Col>
-									<Row>
+									<Row align="center">
 										{status === 'upcoming' ? (
-											<Col>
-												<Text fontSize="xxs" className="font-weight-400">
-													Starts in
-												</Text>
+											<StatusCol>
+												<StatusText fontSize="xxs">Starts in</StatusText>
 												<Text fontSize="xxs">{moment(startBlockDate).diff(moment(), 'days') + 1} days</Text>
-											</Col>
+											</StatusCol>
 										) : status === 'round1' ? (
-											<Col>
-												<Text fontSize="xxs" className="font-weight-400">
-													Round 1
-												</Text>
+											<StatusCol>
+												<StatusText fontSize="xxs">Round 1</StatusText>
 												<Text fontSize="xxs">{moment(round1EndDate).diff(moment(), 'days')} days</Text>
-											</Col>
+											</StatusCol>
 										) : status === 'round2' ? (
-											<Col>
-												<Text fontSize="xxs" className="font-weight-400">
-													Round 2
-												</Text>
+											<StatusCol>
+												<StatusText fontSize="xxs">Round 2</StatusText>
 												<Text fontSize="xxs">{moment(endBlockDate).diff(moment(), 'days')} days</Text>
-											</Col>
+											</StatusCol>
 										) : status === 'saleDone' ? (
-											<Col>
-												<Text fontSize="xxs" className="font-weight-400">
-													Round 2 ended
-												</Text>
-											</Col>
+											<StatusCol>
+												<StatusText fontSize="xxs">Round 2 ended</StatusText>
+											</StatusCol>
 										) : status === 'success' ? (
-											<Col>
-												<Text fontSize="xxs">Successful</Text>
-											</Col>
+											<StatusCol>
+												<StatusText fontSize="xxs">Successful</StatusText>
+											</StatusCol>
 										) : status === 'failed' ? (
-											<Col>
-												<Text fontSize="xxs">Failed</Text>
-											</Col>
+											<StatusCol>
+												<StatusText fontSize="xxs">Failed</StatusText>
+											</StatusCol>
 										) : null}
 									</Row>
 								</Col>
@@ -199,26 +228,34 @@ export const IloCard: FC<IIloCardProps> = ({ data, width }) => {
 									</Row>
 								</Col>
 							</StatusBar>
+							<Spacing vertical="s" />
 							<StatusBar>
 								<Col>
-									<Row align="center" flex={1}>
-										<Icon icon="lock" color="secondaryBrand" height={17} width={17} />
+									<StyledStatusRow align="center" flex={1}>
+										<Icon icon="lock" color="greeny" height={17} width={17} />
 										<Spacing horizontal="xs" />
-										<Text fontSize="m" fontWeight="bold">
+										<Text fontSize="m" fontWeight="bold" style={{ display: 'flex', gap: '5px' }}>
 											{liquidityRatePercent}%
+											<Text fontSize="m" fontWeight="normal">
+												Liquidity
+											</Text>
 										</Text>
-									</Row>
+									</StyledStatusRow>
 								</Col>
 								<Col>
-									<Row align="center" justify="flex-end" flex={1}>
-										<Icon icon="user" color="primaryBrand" height={17} width={17} />
+									<StyledStatusRow align="center" justify="flex-end" flex={1}>
+										<Icon icon="user" color="purple" height={17} width={17} />
 										<Spacing horizontal="xs" />
-										<Text fontSize="m" fontWeight="bold" style={{}}>
-											{numBuyers}
+										<Text fontSize="m" fontWeight="bold" style={{ display: 'flex', gap: '5px' }}>
+											{numBuyers}{' '}
+											<Text fontSize="m" fontWeight="normal">
+												Participants
+											</Text>
 										</Text>
-									</Row>
+									</StyledStatusRow>
 								</Col>
 							</StatusBar>
+							<Spacing vertical="xs" />
 						</CardInfo>
 					</IloDescription>
 					<ProgressBar
@@ -228,6 +265,7 @@ export const IloCard: FC<IIloCardProps> = ({ data, width }) => {
 						backgroundColor="tertiaryBackground"
 					>
 						<StyledProgressBar
+							className="progress_bar"
 							maxHeight
 							maxWidth
 							justify="center"
